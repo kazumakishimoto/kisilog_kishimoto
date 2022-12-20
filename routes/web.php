859daf1bf4a16dc331222fd0ad424ccb812b0bc4
kazumakishimoto <kazuma.kishimoto0114@gmail.com>
2022-12-20 10:38:41 +0900

    [add]web.php追加

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ListController;
-use App\Http\Controllers\InsertController;
-use App\Http\Controllers\DetailController;
-use App\Http\Controllers\UpdateController;
-use App\Http\Controllers\DeleteController;
-use App\Http\Controllers\SearchController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,48 +10,112 @@ use App\Http\Controllers\SearchController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// 1.トップページ
-Route::get('/', function () {
-    return view('menu');
+
+/*
+|--------------------------------------------------------------------------
+| 認証機能
+|--------------------------------------------------------------------------
+*/
+# ユーザー新規登録、ログイン、ログアウト
+Auth::routes();
+
+/*
+|--------------------------------------------------------------------------
+| 一般ユーザー機能
+|--------------------------------------------------------------------------
+*/
+# 一般ユーザー
+Route::prefix('users')->name('users.')->group(function () {
+    // ユーザー詳細
+    Route::get('/{name}', 'UserController@show')->name('show');
 });
 
-// 2.一覧表示
-Route::get('/list', [ListController::class, 'index'])->name('list.index');
+# プロフィール機能
+Route::middleware('auth')->group(function () {
+    Route::prefix('users/{name}')->name('users.')->group(function () {
+        // プロフィール編集
+        Route::get('/edit', 'UserController@edit')->name('edit');
+        // プロフィール更新
+        Route::patch('/update', 'UserController@update')->name('update');
+    });
+});
 
-// 3.登録画面を表示
-Route::get('/insert', function () {
-    return view('insert');
-})->name('insert.index');
+# 口コミ機能(CRUD)
+Route::get('/', 'ArticleController@index')->name('articles.index');
+Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->only(['show']);
 
-// 4.登録処理
-Route::post('/insert', [InsertController::class, 'insert'])->name('insert.insert');
 
-// 5.登録画面へリダイレクト
-Route::get('/insert_redirect', function () {
-    return redirect('/insert');
-})->name('insert.redirect');
+/*
+|--------------------------------------------------------------------------
+| 飲食店ユーザー
+|--------------------------------------------------------------------------
+*/
+# 飲食店ユーザー
+Route::prefix('shops')->name('shops.')->group(function () {
+    // ユーザー詳細
+    Route::get('/{name}', 'ShopController@show')->name('show');
+});
 
-// 6.詳細処理
-Route::get('/detail', [DetailController::class, 'index'])->name('detail.index');
+# プロフィール機能
+Route::middleware('auth')->group(function () {
+    Route::prefix('shops/{name}')->name('shops.')->group(function () {
+        // プロフィール編集
+        Route::get('/edit', 'ShopController@edit')->name('edit');
+        // プロフィール更新
+        Route::patch('/update', 'ShopController@update')->name('update');
+    });
+});
 
-// 7.変更画面を表示
-Route::get('/update', [UpdateController::class, 'index'])->name('update.index');
+# メニュー機能(CRUD)
+Route::get('/', 'MenuController@index')->name('menus.index');
+Route::resource('/menus', 'MenuController')->except(['index', 'show'])->middleware('auth');
+Route::resource('/menus', 'MenuController')->only(['show']);
 
-// 8.変更処理
-Route::post('/update', [UpdateController::class, 'update'])->name('update.update');
+/*
+|--------------------------------------------------------------------------
+| 共通ユーザー
+|--------------------------------------------------------------------------
+*/
+# トップページ
+Route::get('/', function () {
+    return view('menuList');
+});
 
-// 9.削除処理
-Route::get('/delete', [DeleteController::class, 'index'])->name('delete.index');
+# 管理者ユーザー機能
+Route::prefix('administrator')->name('administrator.')->group(function () {
+    // ユーザー詳細
+    Route::get('/{name}', 'AdministratorController@show')->name('show');
+});
 
-// 10.検索処理
-Route::post('/search', [SearchController::class, 'index'])->name('search.index');
+# 予約機能
+Route::prefix('reservation')->name('reservation.')->group(function () {
+    Route::middleware('auth')->group(function () {
+        Route::get('/index', 'ReservationController@index')->name('index');
+        Route::get('/create', 'ReservationController@create')->name('create');
+        Route::post('/store', 'ReservationController@store')->name('store');
+    });
+});
 
-// 11.一覧機能へリダイレクト
-Route::get('/list_redirect', function () {
-    return redirect('/list');
-})->name('list.redirect');
+# クーポン機能
+Route::prefix('coupons')->name('coupons.')->group(function () {
+    Route::middleware('auth')->group(function () {
+        Route::get('/index', 'CouponController@index')->name('index');
+        Route::get('/create', 'CouponController@create')->name('create');
+        Route::post('/store', 'CouponController@store')->name('store');
+    });
+});
 
-// 12.エラー処理
+# バナー機能
+Route::prefix('banners')->name('banners.')->group(function () {
+    Route::middleware('auth')->group(function () {
+        Route::get('/index', 'BannerController@index')->name('index');
+        Route::get('/create', 'BannerController@create')->name('create');
+        Route::post('/store', 'BannerController@store')->name('store');
+    });
+});
+
+# エラー処理
 Route::get('/error', function () {
     return view('error');
 })->name('error.index');
