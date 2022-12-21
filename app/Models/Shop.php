f6bc55cbacdbf430bda22a74fdbc7b789a94558d
kazumakishimoto <kazuma.kishimoto0114@gmail.com>
2022-12-21 11:12:18 +0900

    [fix]モデルのリレーション追記

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Shop extends Model {
-    use HasFactory;
+    // use HasFactory;
+
+    public function menus(): HasMany {
+        return $this->hasMany('App\Models\Menu');
+    }
+
+    public function oupons(): HasMany {
+        return $this->hasMany('App\Models\Coupon');
+    }
+
+    public function banners(): HasMany {
+        return $this->hasMany('App\Models\Banner');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable {
-    use HasFactory;
+    // use HasFactory;
+
+    public function articles(): HasMany {
+        return $this->hasMany('App\Models\Article');
+    }
+
+    public function reservations(): HasMany {
+        return $this->hasMany('App\Models\Reservation');
+    }
 }
