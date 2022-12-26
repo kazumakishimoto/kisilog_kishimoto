c7346a42085c68d95b25852a2cbedd76a3e214e7
kazumakishimoto <kazuma.kishimoto0114@gmail.com>
2022-12-26 13:32:15 +0900

    [fix]ユーザーのシーダー追記

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -7,23 +7,49 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-class UsersTableSeeder extends Seeder
-{
+class UsersTableSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         DB::table('users')->insert([
             'name' => 'user',
             'age' => '30',
             'gender' => '男',
-            'avatar' => 'images/example.png',
+            'avatar' => 'image/example.png',
             'introduction' => 'ユーザーの自己紹介',
             'authority' => 'ユーザー',
             'email' => 'user@example.com',
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'shop',
+            'age' => '30',
+            'gender' => '男',
+            'avatar' => 'image/example.png',
+            'introduction' => '飲食店の自己紹介',
+            'authority' => '飲食店',
+            'email' => 'shop@example.com',
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'age' => '30',
+            'gender' => '男',
+            'avatar' => 'image/example.png',
+            'introduction' => '管理者の自己紹介',
+            'authority' => '管理者',
+            'email' => 'admin@example.com',
             'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
             'created_at' => now(),
